238c0573dcc8a1b5dcdc4d5aec90619430e30f6c
Marcus Reaiche <marcusreaiche@gmail.com>
2023-09-07 15:55:55 -0400

    Implement test_import_data using pytest

--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -1,63 +1,66 @@
-import os
-import logging
-import churn_library_solution as cls
+import pytest
+from file_logger import file_logger
+from constants import DATA_FILEPATH
+from churn_library import (
+	import_data,
+	perform_eda,
+	perform_feature_engineering,
+	encoder_helper,
+	train_models)
 
-logging.basicConfig(
-    filename='./logs/churn_library.log',
-    level = logging.INFO,
-    filemode='w',
-    format='%(name)s - %(levelname)s - %(message)s')
+# Pytest fixtures
+@pytest.fixture(scope="module")
+def data_path():
+	return DATA_FILEPATH
 
-def test_import(import_data):
+
+def test_import_data(data_path):
 	'''
-	test data import - this example is completed for you to assist with the other test functions
+	Test import data
 	'''
 	try:
-		df = import_data("./data/bank_data.csv")
-		logging.info("Testing import_data: SUCCESS")
+		data = import_data(data_path)
+		file_logger.info("Testing import_data: SUCCESS")
 	except FileNotFoundError as err:
-		logging.error("Testing import_eda: The file wasn't found")
+		file_logger.error("Testing import_data: The file wasn't found")
 		raise err
 
 	try:
-		assert df.shape[0] > 0
-		assert df.shape[1] > 0
+		assert data.shape[0] > 0
+		assert data.shape[1] > 0
 	except AssertionError as err:
-		logging.error("Testing import_data: The file doesn't appear to have rows and columns")
+		file_logger.error("Testing import_data:" +
+					      " The file doesn't appear to have rows and columns")
 		raise err
 
 
-def test_eda(perform_eda):
+def test_perform_eda():
 	'''
-	test perform eda function
+	Test perform eda function
 	'''
+	raise NotImplementedError('Implement test_perform_eda')
 
 
-def test_encoder_helper(encoder_helper):
+def test_encoder_helper():
 	'''
-	test encoder helper
+	Test encoder helper
 	'''
+	raise NotImplementedError('Implement test_encoder_helper')
 
 
-def test_perform_feature_engineering(perform_feature_engineering):
+def test_perform_feature_engineering():
 	'''
-	test perform_feature_engineering
+	Test perform_feature_engineering
 	'''
+	raise NotImplementedError('Implement test_perform_feature_engineering')
 
 
-def test_train_models(train_models):
+def test_train_models():
 	'''
-	test train_models
+	Test train_models
 	'''
+	raise NotImplementedError('Implement test_train_models')
 
 
 if __name__ == "__main__":
-	pass
-
-
-
-
-
-
-
-
+	pytest.main([__file__])

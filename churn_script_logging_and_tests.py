0143ee460841ec07e0c8dc97e3120fff87080e4d
Marcus Reaiche <marcusreaiche@gmail.com>
2023-09-07 22:18:00 -0400

    Add test_perform_eda

--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -1,66 +1,109 @@
+import os
+import numpy as np
 import pytest
 from file_logger import file_logger
-from constants import DATA_FILEPATH
+from constants import DATA_FILEPATH, IMG_FILE_EXT
+import churn_library
 from churn_library import (
-	import_data,
-	perform_eda,
-	perform_feature_engineering,
-	encoder_helper,
-	train_models)
+    import_data,
+    perform_eda,
+    perform_feature_engineering,
+    encoder_helper,
+    train_models)
 
 # Pytest fixtures
-@pytest.fixture(scope="module")
+@pytest.fixture(scope='module')
 def data_path():
-	return DATA_FILEPATH
+    return DATA_FILEPATH
+
+@pytest.fixture(scope='module')
+def data_before_eda():
+    return import_data(DATA_FILEPATH)
 
 
 def test_import_data(data_path):
-	'''
-	Test import data
-	'''
-	try:
-		data = import_data(data_path)
-		file_logger.info("Testing import_data: SUCCESS")
-	except FileNotFoundError as err:
-		file_logger.error("Testing import_data: The file wasn't found")
-		raise err
+    '''
+    Test import data
+    '''
+    try:
+        data = import_data(data_path)
+        file_logger.info("Testing import_data: SUCCESS")
+    except FileNotFoundError as err:
+        file_logger.error("Testing import_data: The file wasn't found")
+        raise err
 
-	try:
-		assert data.shape[0] > 0
-		assert data.shape[1] > 0
-	except AssertionError as err:
-		file_logger.error("Testing import_data:" +
-					      " The file doesn't appear to have rows and columns")
-		raise err
+    try:
+        assert data.shape[0] > 0
+        assert data.shape[1] > 0
+    except AssertionError as err:
+        file_logger.error("Testing import_data:" +
+                          " The file doesn't appear to have rows and columns")
+        raise err
 
 
-def test_perform_eda():
-	'''
-	Test perform eda function
-	'''
-	raise NotImplementedError('Implement test_perform_eda')
+def test_perform_eda(data_before_eda, tmp_path, monkeypatch):
+    '''
+    Test perform_eda function
+    '''
+    file_logger.info('Testing perform_eda')
+    # Set temporary directory to save EDA files
+    tmp_images_eda_directory = tmp_path / 'images' / 'eda'
+    tmp_images_eda_directory.mkdir(parents=True)
+    monkeypatch.setattr(churn_library, "IMG_EDA_DIR", tmp_images_eda_directory)
+    try:
+        perform_eda(data_before_eda)
+        # Test that 'Churn' columns was created
+        assert 'Churn' in data_before_eda
+        file_logger.info('"Churn" column was created')
+    except AssertionError as err:
+        file_logger.error('"Churn" column was not created')
+        raise err
+    try:
+        # Test that 'Churn' column has only zeros and ones
+        assert set(data_before_eda.Churn.value_counts().index).issubset({0, 1})
+        file_logger.info('"Churn" column has only 0\'s and 1\'s')
+    except AssertionError as err:
+        file_logger.error('"Churn" column has values not in {0, 1}')
+        raise err
+    try:
+        # Check that the five images were saved in the temporary directory
+        expected_images = set([
+            'total_transaction_distribution.png',
+            'customer_age_distribution.png',
+            'churn_distribution.png',
+            'marital_status_distribution.png',
+            'heatmap.png'])
+        saved_images = set([
+            file for file in os.listdir(tmp_images_eda_directory)
+            if file.endswith(IMG_FILE_EXT)])
+        assert saved_images == expected_images
+        file_logger.error('Expected image files were saved to disk')
+    except AssertionError as err:
+        file_logger.error('Saved image files do not agree with expected images')
+        raise err
+    file_logger.info('Testing perform_eda - SUCCESS')
 
 
 def test_encoder_helper():
-	'''
-	Test encoder helper
-	'''
-	raise NotImplementedError('Implement test_encoder_helper')
+    '''
+    Test encoder helper
+    '''
+    raise NotImplementedError('Implement test_encoder_helper')
 
 
 def test_perform_feature_engineering():
-	'''
-	Test perform_feature_engineering
-	'''
-	raise NotImplementedError('Implement test_perform_feature_engineering')
+    '''
+    Test perform_feature_engineering
+    '''
+    raise NotImplementedError('Implement test_perform_feature_engineering')
 
 
 def test_train_models():
-	'''
-	Test train_models
-	'''
-	raise NotImplementedError('Implement test_train_models')
+    '''
+    Test train_models
+    '''
+    raise NotImplementedError('Implement test_train_models')
 
 
 if __name__ == "__main__":
-	pytest.main([__file__])
+    pytest.main([__file__])
